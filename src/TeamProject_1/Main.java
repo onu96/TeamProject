6a2ec2c873acb0616df97231558304d03ff2d202
schlanger <schlanger@naver.com>
2025-07-06 12:35:41 +0900

    내가 한 최종 병합 오류 해결 커밋

--- src/TeamProject_1/Main.java
+++ src/TeamProject_1/Main.java
@@ -3,11 +3,8 @@ package TeamProject_1;
 public class Main {
 
 	public static void main(String[] args) {
-<<<<<<< HEAD
 		System.out.println("프로젝트 시작합니다."); // main에서 수정
-=======
 		System.out.println("Git 연습을 시작합니다!"); // welcome-message에서 수정
->>>>>>> welcome-message
 		System.out.println("커밋 테스트 중입니다.");
 		System.out.println("환영합니다! Git 브랜치 연습 중입니다.");
 	}
