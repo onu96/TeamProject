e4c6f305e517424232a1526e3df53abb0389c19b
schlanger <schlanger@naver.com>
2025-07-06 12:02:14 +0900

    환영 메세지 출력기능 추가

--- src/TeamProject_1/Main.java
+++ src/TeamProject_1/Main.java
@@ -5,6 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("프로젝트에 Git을 도입했습니다.");
 		System.out.println("커밋 테스트 중입니다.");
+		System.out.println("환영합니다! Git 브랜치 연습 중입니다.");
 	}
 
 }
