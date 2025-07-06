4a0226a00a804eed9115821d6787b9941b5e566e
schlanger <schlanger@naver.com>
2025-07-06 12:16:21 +0900

    welcome-message 브랜치에서 같은 줄 수정

--- src/TeamProject_1/Main.java
+++ src/TeamProject_1/Main.java
@@ -3,7 +3,7 @@ package TeamProject_1;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("프로젝트에 Git을 도입했습니다.");
+		System.out.println("Git 연습을 시작합니다!"); // welcome-message에서 수정
 		System.out.println("커밋 테스트 중입니다.");
 		System.out.println("환영합니다! Git 브랜치 연습 중입니다.");
 	}
