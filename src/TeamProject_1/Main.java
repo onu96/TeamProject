1dd2577e5c285d452382bcea9c64f3726cb7d7cb
schlanger <schlanger@naver.com>
2025-07-06 11:30:43 +0900

    ✏️ Main.java 수정 - 로그 출력 추가

--- src/TeamProject_1/Main.java
+++ src/TeamProject_1/Main.java
@@ -3,7 +3,8 @@ package TeamProject_1;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("프로젝트 시작");
+		System.out.println("프로젝트에 Git을 도입했습니다.");
+		System.out.println("커밋 테스트 중입니다.");
 	}
 
 }
